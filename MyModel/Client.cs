e17035c02798d32574d2e4fd164aa1c788368bdc
Omar <Omar@DESKTOP-T0PFLV5>
2019-04-11 14:47:28 +0100

    second commit
    the project

---
+++ MyModel/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyModel
+{
+    public class Client
+    {
+        int id;
+        string FirstName;
+        string LastName;
+
+        
+
+        public int Id { get => id; set => id = value; }
+        public string Firstname { get => FirstName; set => FirstName = value; }
+        public string Lastname { get => LastName; set => LastName = value; }
+        public override string ToString()
+        {
+            return Firstname + " " + LastName;
+        }
+    }
+}
